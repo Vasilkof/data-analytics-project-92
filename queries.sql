a27e7f8b503be301589d034e1990d06abee6286e
Vasilkof <63141276+Vasilkof@users.noreply.github.com>
2026-02-19 13:53:32 +0200

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -51,3 +51,46 @@ ORDER BY
     EXTRACT(ISODOW FROM s.sale_date),
     seller;
 
+-- Report : Number of customers  age groups 
+SELECT
+    CASE
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        ELSE '40+'
+    END AS age_category,
+    COUNT(*) AS age_count
+FROM customers
+GROUP BY age_category
+ORDER BY age_category;
+
+
+-- Report : Number of unique customers
+SELECT
+    TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
+    COUNT(DISTINCT s.customer_id) AS total_customers,
+    FLOOR(SUM(p.price * s.quantity)) AS income
+FROM sales s
+JOIN products p ON s.product_id = p.product_id
+GROUP BY selling_month
+ORDER BY selling_month;
+
+
+-- Report : Customers  special offer 
+
+SELECT
+    TRIM(c.first_name || ' ' || c.last_name) AS customer,
+    s.sale_date,
+    TRIM(e.first_name || ' ' || e.last_name) AS seller
+FROM (
+    SELECT customer_id, MIN(sale_date) AS first_sale_date
+    FROM sales
+    GROUP BY customer_id
+) first_sales
+JOIN sales s
+    ON s.customer_id = first_sales.customer_id
+    AND s.sale_date = first_sales.first_sale_date
+JOIN products p ON s.product_id = p.product_id
+JOIN customers c ON s.customer_id = c.customer_id
+JOIN employees e ON s.sales_person_id = e.employee_id
+WHERE p.price = 0
+ORDER BY s.customer_id;
