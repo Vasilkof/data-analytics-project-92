65615fd98db9295a824f6af1b32033692dd92314
Vasilkof <63141276+Vasilkof@users.noreply.github.com>
2026-03-01 14:50:53 +0200

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -16,6 +16,7 @@ ORDER BY income DESC
 LIMIT 10;
 -- Report 2: Sellers with average 
 
+
 SELECT
     TRIM(e.first_name || ' ' || e.last_name) AS seller,
     FLOOR(AVG(p.price * s.quantity)) AS average_income
@@ -93,4 +94,4 @@ JOIN products p ON s.product_id = p.product_id
 JOIN customers c ON s.customer_id = c.customer_id
 JOIN employees e ON s.sales_person_id = e.employee_id
 WHERE p.price = 0
-ORDER BY s.customer_id;
+ORDER BY customer, sale_date, seller;
