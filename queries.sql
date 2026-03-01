61f88b4ce751c572101aa85722cd5ce844921e12
Vasilkof <63141276+Vasilkof@users.noreply.github.com>
2026-03-01 15:36:39 +0200

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,165 +1,171 @@
 -- Report: Total customers
-SELECT count(*) AS customers_count
-FROM customers;
+select
+    count(*) as customers_count
+from customers;
+
 
 -- Report 1: Top 10 sellers by income
-SELECT
-    TRIM(e.first_name || ' ' || e.last_name) AS seller,
-    COUNT(s.sales_id) AS operations,
-    FLOOR(SUM(p.price * s.quantity)) AS income
-FROM sales AS s
-INNER JOIN employees AS e
-    ON s.sales_person_id = e.employee_id
-INNER JOIN products AS p
-    ON s.product_id = p.product_id
-GROUP BY
-    e.first_name,
-    e.last_name
-ORDER BY
-    income DESC
-LIMIT 10;
-
-
--- Report 2
-SELECT
-    TRIM(e.first_name || ' ' || e.last_name) AS seller,
-    FLOOR(
-        AVG(
+select
+    trim(e.first_name || ' ' || e.last_name) as seller,
+    count(s.sales_id) as operations,
+    floor(
+        sum(
             p.price * s.quantity
         )
-    ) AS average_income
-FROM sales AS s
-INNER JOIN employees AS e
-    ON s.sales_person_id = e.employee_id
-INNER JOIN products AS p
-    ON s.product_id = p.product_id
-GROUP BY
+    ) as income
+from sales as s
+inner join employees as e
+    on s.sales_person_id = e.employee_id
+inner join products as p
+    on s.product_id = p.product_id
+group by
     e.first_name,
     e.last_name
-HAVING
-    AVG(
+order by
+    income desc
+limit 10;
+
+
+-- Report 2: Sellers with average income below overall sellers average
+select
+    trim(e.first_name || ' ' || e.last_name) as seller,
+    floor(
+        avg(
+            p.price * s.quantity
+        )
+    ) as average_income
+from sales as s
+inner join employees as e
+    on s.sales_person_id = e.employee_id
+inner join products as p
+    on s.product_id = p.product_id
+group by
+    e.first_name,
+    e.last_name
+having
+    avg(
         p.price * s.quantity
     ) < (
-        SELECT
-            AVG(sub.seller_avg)
-        FROM (
-            SELECT
-                AVG(
+        select
+            avg(sub.seller_avg) as avg_seller_avg
+        from (
+            select
+                avg(
                     p2.price * s2.quantity
-                ) AS seller_avg
-            FROM sales AS s2
-            INNER JOIN products AS p2
-                ON s2.product_id = p2.product_id
-            GROUP BY
+                ) as seller_avg
+            from sales as s2
+            inner join products as p2
+                on s2.product_id = p2.product_id
+            group by
                 s2.sales_person_id
-        ) AS sub
+        ) as sub
     )
-ORDER BY
-    average_income ASC;
+order by
+    average_income asc;
 
 
--- Report 3
-SELECT
-    TRIM(e.first_name || ' ' || e.last_name) AS seller,
-    TRIM(
-        TO_CHAR(
+-- Report 3: Sellers income by day of week
+select
+    trim(e.first_name || ' ' || e.last_name) as seller,
+    trim(
+        to_char(
             s.sale_date,
             'day'
         )
-    ) AS day_of_week,
-    FLOOR(
-        SUM(
+    ) as day_of_week,
+    floor(
+        sum(
             p.price * s.quantity
         )
-    ) AS income
-FROM sales AS s
-INNER JOIN employees AS e
-    ON s.sales_person_id = e.employee_id
-INNER JOIN products AS p
-    ON s.product_id = p.product_id
-GROUP BY
+    ) as income
+from sales as s
+inner join employees as e
+    on s.sales_person_id = e.employee_id
+inner join products as p
+    on s.product_id = p.product_id
+group by
     e.first_name,
     e.last_name,
-    TRIM(
-        TO_CHAR(
+    trim(
+        to_char(
             s.sale_date,
             'day'
         )
     ),
-    EXTRACT(
-        ISODOW FROM s.sale_date
+    extract(
+        isodow from s.sale_date
     )
-ORDER BY
-    EXTRACT(
-        ISODOW FROM s.sale_date
+order by
+    extract(
+        isodow from s.sale_date
     ),
     seller;
 
 
--- Report 4
-SELECT
-    CASE
-        WHEN c.age BETWEEN 16 AND 25 THEN '16-25'
-        WHEN c.age BETWEEN 26 AND 40 THEN '26-40'
-        ELSE '40+'
-    END AS age_category,
-    COUNT(*) AS age_count
-FROM customers AS c
-GROUP BY
+-- Report: Number of customers in age groups
+select
+    case
+        when c.age between 16 and 25 then '16-25'
+        when c.age between 26 and 40 then '26-40'
+        else '40+'
+    end as age_category,
+    count(*) as age_count
+from customers as c
+group by
     age_category
-ORDER BY
+order by
     age_category;
 
 
--- Report 5
-SELECT
-    TO_CHAR(
+-- Report: Unique customers and income by month
+select
+    to_char(
         s.sale_date,
         'YYYY-MM'
-    ) AS selling_month,
-    COUNT(
-        DISTINCT s.customer_id
-    ) AS total_customers,
-    FLOOR(
-        SUM(
+    ) as selling_month,
+    count(
+        distinct s.customer_id
+    ) as total_customers,
+    floor(
+        sum(
             p.price * s.quantity
         )
-    ) AS income
-FROM sales AS s
-INNER JOIN products AS p
-    ON s.product_id = p.product_id
-GROUP BY
-    TO_CHAR(
+    ) as income
+from sales as s
+inner join products as p
+    on s.product_id = p.product_id
+group by
+    to_char(
         s.sale_date,
         'YYYY-MM'
     )
-ORDER BY
+order by
     selling_month;
 
 
--- Report 6
-SELECT
+-- Report: Customers whose first purchase was during a special offer (price = 0)
+select
+    trim(c.first_name || ' ' || c.last_name) as customer,
     so.sale_date,
-    TRIM(c.first_name || ' ' || c.last_name) AS customer,
-    TRIM(e.first_name || ' ' || e.last_name) AS seller
-FROM (
-    SELECT DISTINCT ON (s.customer_id)
+    trim(e.first_name || ' ' || e.last_name) as seller
+from (
+    select distinct on (s.customer_id)
         s.customer_id,
         s.sale_date,
         s.product_id,
         s.sales_person_id
-    FROM sales AS s
-    ORDER BY
+    from sales as s
+    order by
         s.customer_id,
         s.sale_date
-) AS so
-INNER JOIN products AS p
-    ON so.product_id = p.product_id
-INNER JOIN customers AS c
-    ON so.customer_id = c.customer_id
-INNER JOIN employees AS e
-    ON so.sales_person_id = e.employee_id
-WHERE
+) as so
+inner join products as p
+    on so.product_id = p.product_id
+inner join customers as c
+    on so.customer_id = c.customer_id
+inner join employees as e
+    on so.sales_person_id = e.employee_id
+where
     p.price = 0
-ORDER BY
+order by
     so.customer_id;
